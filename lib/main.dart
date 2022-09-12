27993282be05a6d298e35f08e1831fd5c357a6ae
KyawThu082 <kyawthu082@gmail.com>
2022-09-12 09:37:51 +0630

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(
+        child: Column(children: <Widget>[
+          Card(
+            color: Colors.blue,
+            child: Container(
+                width: double.infinity,
+                child: Center(child: Text('CHART!'))),
+            elevation: 5,
+          ),
+          Card(
+            child: Text('LIST OF TX!'),
+          ),
+        ],
+        ),
+      ),
+    );
+  }
+}
